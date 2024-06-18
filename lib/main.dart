60b8d0efdeaf54d207df7b3c1546cdbf39bd5fea
Animesh Bhattarai <animesh.bhattarai2019@gmail.com>
2024-06-18 23:11:57 +0545

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
-import 'package:quick_cart/themes/light_mode.dart'; 
+import 'models/shop.dart';
+import 'pages/cart_page.dart';
+import 'pages/shop_page.dart';
+import 'themes/light_mode.dart';
+import 'package:provider/provider.dart'; 
 import 'pages/intro_page.dart';
 
 // Entry point of the app
@@ -23,7 +25,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // Disable the debug banner
       home: const IntroPage(), // Set IntroPage as the home page
-      theme: lightMode, // Apply the light mode theme
+      theme: lightMode,
+      routes: {
+        '/intro_page': (context) => const IntroPage(),
+        '/shop_page': (context) => const ShopPage(),
+        '/cart_page': (context) => const CartPage(),
+      } // Apply the light mode theme
     );
   }
 }
